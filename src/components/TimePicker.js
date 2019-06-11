9bf035a2842d426d9c6afe1c2f637366c29234c1
Ádám András <adm.andras@gmail.com>
2019-06-11 20:43:42 +0200

    TimePicker updated

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format } from 'date-fns';
+import { format, getHours, getMinutes, getSeconds } from 'date-fns';
 
 import './styles/TimePicker.scss';
 
@@ -15,11 +15,12 @@ const TimePicker = ({ id, select, selected }) => {
 	const className = 'neotimepicker-picker';
 
 	// Render picker lists
-	const renderList = amount => {
+	const renderList = (amount, selectedValue) => {
 		const items = [];
 		for (let i = 0; i < amount; i++) {
+			const cN = className + '__body-list-item' + (selectedValue !== i ? '' : ' neotimepicker--selected');
 			items.push(
-				<div>{i}</div>
+				<div key={i} className={cN}>{i < 10 ? '0' + i : i}</div>
 			);
 		}
 		return items;
@@ -32,9 +33,9 @@ const TimePicker = ({ id, select, selected }) => {
             <input className={className + '__input'} autoFocus={true} defaultValue={time} placeholder="hh:mm:ss" />
          </div>
          <div className={className + '__body'}>
-				<div className={className + '__body-list'}>{renderList(24)}</div>
-				<div className={className + '__body-list'}>{renderList(60)}</div>
-				<div className={className + '__body-list'}>{renderList(60)}</div>
+				<div className={className + '__body-list'}>{renderList(24, getHours(selected))}</div>
+				<div className={className + '__body-list'}>{renderList(60, getMinutes(selected))}</div>
+				<div className={className + '__body-list'}>{renderList(60, getSeconds(selected))}</div>
 			</div>
       </div>
 	);
